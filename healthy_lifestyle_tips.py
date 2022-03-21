d4dbf6f3131bf07da193c283d9afcef954301c4c
Dennis thomas <52597699+denz647@users.noreply.github.com>
2022-03-21 11:14:24 +0530

    Update healthy_lifestyle_tips.py

--- healthy_lifestyle_tips.py
+++ healthy_lifestyle_tips.py
@@ -2,5 +2,11 @@
 Add healthy life style tips in a list.
 '''
 
-def healthy_tips_suggester():
-    pass
+import random
+
+Healthy_tips =['Eat a balanced diet', 'Add variety to your meals',
+'Stay well hydrated','Remember to drink 8 glasses of water a day','Exercise regularly','Get enough good sleep ',
+'Limit your alcohol intake','Do not smoke','Protect yourself from the sun','Wash your hands']
+
+rand=random.choice(Healthy_tips)
+print(rand)
