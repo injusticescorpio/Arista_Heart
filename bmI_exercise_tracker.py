7312c1387ad0b528882404d9cef482d8591bbb94
injusticescorpio <arjunvishnu2000@gmail.com>
2022-03-22 22:03:31 +0530

    resolved some bugs

--- bmI_exercise_tracker.py
+++ bmI_exercise_tracker.py
@@ -13,7 +13,7 @@ class BMI:
         self.weight_details={
 
             'Underweight':"If your BMI is less than 18.5, then it means that you are underweight. To avoid health complications that may arise due to low levels of body fat, you need to put on more weight. Make sure you get in touch with your doctor or a dietician for professional insight and advice.",
-                'Healthy':"If you have a BMI that falls in between 18.5 to 22.9, then it means that you have a healthy weight in relation to your height. This is your normal BMI range. When you maintain a healthy level of body fat, it means that you have a much lower risk of developing health complications.",
+                'Normal Weight':"If you have a BMI that falls in between 18.5 to 22.9, then it means that you have a healthy weight in relation to your height. This is your normal BMI range. When you maintain a healthy level of body fat, it means that you have a much lower risk of developing health complications.",
             'Overweight':"If you have a BMI that falls in between 23.0 to 24.9, it means that you are overweight. In other words, you have a higher than ideal level of body fats considering your height. In such cases, it is important that you lose some amount of weight in order to improve your health. It is recommended that you talk to a doctor or dietician for professional advice.",
             'Obese':"If your BMI is more than 25.0, then it indicates that you are obese, or in other words, heavily overweight. It is far from your ideal BMI and it means that you have way too much body fat in relation to your height, and this can pose serious health risks. It is important that you lose weight for health reasons. Make sure to contact your doctor or dietician for professional advice in such a situation."
 
@@ -50,11 +50,10 @@ class BMI:
 
 
 
-bmi=BMI(1.94,100)
+bmi=BMI(1.94,80)
 print(bmi.bmi_calculation())
 print(bmi.weight_category())
-bmi.play_video('https://www.youtube.com/watch?v=H3jJ29oE8Zg')
-
+bmi.play_category()
 
 
 
