e2a9aecf0eaa0791f8f0be46f19893248dd291b9
injusticescorpio <arjunvishnu2000@gmail.com>
2022-03-28 21:11:09 +0530

    added some changes in pillremainder

--- Pill_remainder_advanced/patient_details.py
+++ Pill_remainder_advanced/patient_details.py
@@ -12,5 +12,10 @@ def get_all_details():
         curr.execute("SELECT * FROM patient")
     curr.execute("SELECT * FROM patient")
     print(curr.fetchall())
+    '''
+    here we can call the pill remainder class and this python file runs according to the condition provided by the user 
+    to set the pills so he/she will get reminder either by an sms or by a call or by a whatsapp message.
+    so all together total 2 server + 2 python file (1 for pill_remainder and 1 for ambulanceService) needed to run for its complete operation
+    '''
 create_table()
 get_all_details()
