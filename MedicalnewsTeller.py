7e32327a2ebe31b32526d92e17ff40e796bd7f5e
denz647 <denz8572@gmail.com>
2022-03-31 23:34:01 +0530

    Adding MedicalnewsTeller

---
+++ MedicalnewsTeller.py
@@ -0,0 +1,30 @@
+
+import requests
+import json
+import time
+import os
+
+
+def Medi_news():
+    url = "https://newsapi.org/v2/top-headlines?"
+    parameters = {
+        'q'       :'health news',
+        'category': 'health',
+        'language': 'en',
+        'country': 'in',
+        'apiKey': '0650237c3f21411ea4f27185f0747e09'
+    }
+
+
+    response = requests.get(url, params=parameters)
+    response_json = response.json()
+    # pprint(response_json)
+
+    for i in response_json['articles']:
+        print(str(i['title']),":","\n",str(i['description']),"\n")
+        print("For More Details :",str(i['url']),"\n")
+        time.sleep(2)
+
+Medi_news()
+
+
