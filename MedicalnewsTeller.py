76d8c92269b4e102483eda964bad23c1eb060a4b
injusticescorpio <arjunvishnu2000@gmail.com>
2022-04-09 20:56:34 +0530

    modified medical news teller

--- MedicalnewsTeller.py
+++ MedicalnewsTeller.py
@@ -1,26 +1,20 @@
 import requests
 import os
 
+url = "https://newsapi.org/v2/top-headlines?"
+parameters = {'q': 'health','category': 'health','language': 'en',
+                'country': 'in', 'apiKey': os.environ['news_api']
+}
 
-def Medi_news():
-    url = "https://newsapi.org/v2/top-headlines?"
-    parameters = {
-        'q': 'health news',
-        'category': 'health',
-        'language': 'en',
-        'country': 'in',
-        'apiKey': 'API-KEY'
-    }
+class Medicine_News_Teller:
+    def __init__(self):
+        self.response = requests.get(url, params=parameters).json()
+    def details(self):
+        self.seperator='.\n'
+        self.news_details =[]
+        for details in self.response['articles']:
+            self.news_details.append(f"⚫ {details['title']} => \n {self.seperator.join(details['description'].split('.'))} \nFor more Information visit {details['url']}")
+        return ('\n'.join(self.news_details))
 
-    response = requests.get(url, params=parameters)
-    response_json = response.json()
-    # pprint(response_json)
-
-    for i in response_json['articles']:
-        print(str(i['title']), ":", "\n", str(i['description']), "\n")
-        print("For More Details", str(i['url']), "\n")
-
-
-Medi_news()
-
-
+m=Medicine_News_Teller()
+m.details()
