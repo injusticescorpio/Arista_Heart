11df7bbc015350018f6775dd089c7733b111e1c5
injusticescorpio <arjunvishnu2000@gmail.com>
2022-04-09 15:42:58 +0530

    started working on disease_precaution_description

--- Disease Prediction from symptoms/disease_precaution_description.py
+++ Disease Prediction from symptoms/disease_precaution_description.py
@@ -3,3 +3,7 @@
 class Disease_Precaution_Description:
     def __init__(self,disease):
         self.disease=disease
+    def precaution(self):
+        pass
+    def description(self):
+        pass
