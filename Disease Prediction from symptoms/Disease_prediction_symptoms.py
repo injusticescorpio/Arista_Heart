75c8f4dec45fe18c926857d1f4aa89e69e4a3249
injusticescorpio <arjunvishnu2000@gmail.com>
2022-04-09 15:59:45 +0530

    edited disease prediction from symptoms

---
+++ Disease Prediction from symptoms/Disease_prediction_symptoms.py
@@ -0,0 +1,14 @@
+from . import disease_precaution_description
+import pandas as pd
+
+class Disease_Prediction_Symptom:
+    def __init__(self,name,symptoms,another_symptom):
+        self.name = name
+        self.symptoms =symptoms
+        self.another_symptom =another_symptom
+    def disease_prediction(self):
+        pass
+    def disease_precaution(self):
+        pass
+    def disease_description(self):
+        pass
